c48217fa0e815c84d12dd0a87fd5f3d620730895
Muhannad <muhannadalsrahen@gmail.com>
2025-05-15 14:33:58 -0700

    fix(ui): change the create post icon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,14 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { Home, Search, Users, Bookmark, PenSquare } from 'lucide-react'
+import { Home, Search, Users, Bookmark, SquarePlus } from 'lucide-react'
 
 export const navigation = [
   { name: 'Home', icon: Home, href: '/' },
   { name: 'Explore', icon: Search, href: '/explore' },
   { name: 'Users', icon: Users, href: '/users' },
   { name: 'Saved', icon: Bookmark, href: '/saved-post' },
-  { name: 'Create Post', icon: PenSquare, href: '/create-post' },
+  { name: 'Create Post', icon: SquarePlus, href: '/create-post' },
 ]
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
