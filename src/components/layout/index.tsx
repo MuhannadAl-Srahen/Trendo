3aaffe945d9a93c19ef46173bbaf8d14edf7fb78
Muhannad <muhannadalsrahen@gmail.com>
2025-05-15 17:16:52 -0700

    feat(ui): add the topbar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,13 @@
 import BottomBar from './BottomBar'
 import SideBar from './SideBar'
+import TopBar from './TopBar'
+
 import { Outlet } from 'react-router'
 
 function Layout() {
   return (
     <>
-     
+      <TopBar />
       <SideBar />
       <BottomBar />
       <main className='my-16 w-full p-6 md:my-0 md:ml-64 md:p-6'>
