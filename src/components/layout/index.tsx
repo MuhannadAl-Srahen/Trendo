f30c2c5d907716271b774888b994bb094842e3d2
Muhannad <muhannadalsrahen@gmail.com>
2025-05-15 17:08:43 -0700

    feat(ui): add the bottmbar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,14 @@
+import BottomBar from './BottomBar'
 import SideBar from './SideBar'
 import { Outlet } from 'react-router'
 
 function Layout() {
   return (
     <>
+     
       <SideBar />
-      <main className='w-full p-6 md:ml-64'>
+      <BottomBar />
+      <main className='my-16 w-full p-6 md:my-0 md:ml-64 md:p-6'>
         <Outlet />
       </main>
     </>
