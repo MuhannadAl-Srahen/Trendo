c614196c138678af3ce5a5eb46b27cefb5b75da7
Abdulrahman <abdkalnaser@gmail.com>
2025-05-17 14:19:03 +0300

    feat: Create an user page with some feature like search bar that make you able to finds others by thier name abd username and the design mobile responsive

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -58,7 +58,7 @@ const filteredUsers = users.filter(
         <DropdownMenu>All</DropdownMenu>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {filteredUsers.map((user, index) => (
           <div
             key={index}
